090dd27b8ebceaa8273bbd4811f21c6bb666bf9b
Akhdan Fadhilah <akhdan@akhdan.dev>
2026-03-25 19:21:58 +0900

    11_hashmaps book ch8.3

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,15 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        //
+        // good alternative here: https://qiita.com/hystcs/items/75183bcf38bf95cc2ce0
+        // or_insert here is my first approach; better yet is to use or_default; see docs
+        let team_1_scores = scores.entry(team_1_name).or_insert(TeamScores { goals_scored: 0, goals_conceded: 0 });
+        team_1_scores.goals_scored += team_1_score;
+        team_1_scores.goals_conceded += team_2_score;
+        let team_2_scores = scores.entry(team_2_name).or_insert(TeamScores { goals_scored: 0, goals_conceded: 0 });
+        team_2_scores.goals_scored += team_2_score;
+        team_2_scores.goals_conceded += team_1_score;
     }
 
     scores
