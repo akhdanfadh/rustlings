e15602cc83b27e502009a6aa9f4951ceac5dee0e
Akhdan Fadhilah <akhdan@akhdan.dev>
2026-03-25 21:37:41 +0900

    12_options book ch6.1

--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -10,7 +10,7 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if-let statement whose value is `Some`.
-        word = optional_target {
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -23,13 +23,24 @@ mod tests {
         for i in 1..=range {
             optional_integers.push(Some(i));
         }
+        // after this, it will be:
+        // [None, Some(1), Some(2), ..., Some(10)]
 
         let mut cursor = range;
 
         // TODO: Make this a while-let statement. Remember that `Vec::pop()`
         // adds another layer of `Option`. You can do nested pattern matching
         // in if-let and while-let statements.
-        integer = optional_integers.pop() {
+        //
+        // so the popped result type is Option<Option<i8>>
+        // minimal flow example:
+        // - 1st iter: vec is [None, Some(1)]
+        //  pop() removes Some(1) -> returns Some(Some(1))
+        //  matches Some(Some(i)) -> i = 1
+        // - 2nd iter: vec is [None]
+        //  pop() removes None -> returns Some(None)
+        //  does not match Some(Some(i))? -> loop exits
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
